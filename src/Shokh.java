a479e73293e39f82c5752d2ae9d15e1a02124557
Azbast <aziztolipov03@gmail.com>
2025-05-15 13:28:04 +0500

    fist version

---
+++ src/Shokh.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Shokh {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        System.out.println("Assalomu Alaykum ");
+    }
+}
