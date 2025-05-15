d9b6bf4cbfc058c3c1a85bdbc4de260d3c943592
samandar <samandarsoatmurotov2327@gmail.com>
2025-05-15 13:55:37 +0500

    abdhiahidhiadhihd

--- src/Shokh.java
+++ src/Shokh.java
@@ -4,6 +4,6 @@ public class Shokh {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-        System.out.println("Assalomu Alaykum ");
+        /// sknfkswfowjfowsbdjs
     }
 }
