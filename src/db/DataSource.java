ad0cdb12f57d814d4084e666fdd8aef78903c576
Azbast <aziztolipov03@gmail.com>
2025-05-15 13:48:30 +0500

    ikkinchi version

--- src/db/DataSource.java
+++ src/db/DataSource.java
@@ -1,6 +1,16 @@
 package db;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class DataSource {
     //salom dunyo ishlar yaxshimi
+    public static void main(String[] args) {
+
+        ArrayList list = new ArrayList();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+    }
 
 }
