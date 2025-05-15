ab5672005d4a2001ed79378dbf7819152372e533
samandar <samandarsoatmurotov2327@gmail.com>
2025-05-15 14:22:44 +0500

    uchirdim

--- src/Shokh.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Shokh {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        /// sknfkswfowjfowsbdjs
-    }
-}
